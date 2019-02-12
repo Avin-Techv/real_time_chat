d2de46a8edd90f3216929b7b09bb68f564d94449
Avin-Techv <avin.mathew@techversantinc.com>
2019-02-12 18:34:00 +0530

    added registration, reset password templates

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import AuthenticationHome
+from .views import AuthenticationHome, RegistrationView, ResetPassword
 
 # Create your views here.
 
 urlpatterns = [
-    path('', AuthenticationHome.as_view(), name="authentication_home")
+    path('', AuthenticationHome.as_view(), name="authentication_home"),
+    path('registration/', RegistrationView.as_view(), name="registration"),
+    path('reset_password/', ResetPassword.as_view(), name="reset_password")
 ]
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -3,3 +3,11 @@ from django.views.generic import TemplateView
 
 class AuthenticationHome(TemplateView):
     template_name = "authentication/base.html"
+
+
+class RegistrationView(TemplateView):
+    template_name = "authentication/registration.html"
+
+
+class ResetPassword(TemplateView):
+    template_name = "authentication/reset_password.html"
